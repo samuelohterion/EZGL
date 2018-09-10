a9792a13d590dc8feebbfdf5ff35642c5b64da57
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2018-09-10 15:12:02 +0200

    tetrahedron

--- glprojects/dummy/dummy.cpp
+++ glprojects/dummy/dummy.cpp
@@ -6,128 +6,127 @@ GLProject ( p_name, p_vcd ),
 m ( glm::mat4( 1.f ) ),
 v ( m ),
 p ( m ),
-t ( new Triangle ( glm::vec3 ( 1.f, 0.f, 0.f ), glm::vec3 ( 0.f, 1.f,0.f ), glm::vec3 ( 0.f, 0.f, 1.f ) ) ) {
+tetrahedron(
+    Triangle ( glm::vec3 ( -0.4142136, -0.4142136, -0.4142136 ), glm::vec3 ( 1.f, 0.f,0.f ), glm::vec3 ( 0.f, 1.f, 0.f ) ),
+    Triangle ( glm::vec3 ( -0.4142136, -0.4142136, -0.4142136 ), glm::vec3 ( 0.f, 1.f,0.f ), glm::vec3 ( 0.f, 0.f, 1.f ) ),
+    Triangle ( glm::vec3 ( -0.4142136, -0.4142136, -0.4142136 ), glm::vec3 ( 0.f, 0.f,1.f ), glm::vec3 ( 1.f, 0.f, 0.f ) ),
+    Triangle ( glm::vec3 ( 1.f, 0.f, 0.f ), glm::vec3 ( 0.f, 1.f,0.f ), glm::vec3 ( 0.f, 0.f, 1.f ) ) ) {
 
-	t->divide ( 6 );
+    tetrahedron . divide ( 3 );
 }
 
 Dummy::~Dummy ( ){
 
-	if ( t )
-
-		delete t;
-
-	t = nullptr;
 }
 
 void
 Dummy::init ( ) {
 
-	glClearColor ( .0f, .0f, .0f, 1.f );
+    glClearColor ( .0f, .0f, .0f, 1.f );
 
-	// frame buffers
-	{
-	}
+    // frame buffers
+    {
+    }
 
-	// textures
-	{
-	}
+    // textures
+    {
+    }
 
-	// vertex arrays
-	{
-		// V-DUMMY-TRIANGLE
-		{
-			GLR::VertexArray
-			& va = glr.vertices ( "V-DUMMY-TRIANGLE" ).
-				attrib ( "vertex", 0, 3 ).
-				setUsage ( GL_DYNAMIC_DRAW );
+    // vertex arrays
+    {
+        // V-DUMMY-TRIANGLE
+        {
+            GLR::VertexArray
+            & va = glr.vertices ( "V-DUMMY-TRIANGLE" ).
+                attrib ( "vertex", 0, 3 ).
+                setUsage ( GL_DYNAMIC_DRAW );
 
-			t->fill ( va );
+            tetrahedron.fill ( va );
 
-			va << GLR::VertexArray::Object ( 0, va.vertexCount ( ), GL_TRIANGLES );
-		}
-	}
+            va << GLR::VertexArray::Object ( 0, va.vertexCount ( ), GL_TRIANGLES );
+        }
+    }
 
-	// index arrays
-	{
-	}
+    // index arrays
+    {
+    }
 
-	// shaders
-	{
-		//S-DUMMY-TRIANGLE"
-		{
-			glr.shader (
-				"S-DUMMY-TRIANGLE",
+    // shaders
+    {
+        //S-DUMMY-TRIANGLE"
+        {
+            glr.shader (
+                "S-DUMMY-TRIANGLE",
 
-				//Vertex Shader
-				"#version 330 core\n"
-				"layout( location = 0 ) in vec3 vertex;\n"
-				"uniform mat4 mvp;\n"
-				"out VS2FS {\n"
-				"	vec3 color;\n"
-				"} vs2fs;\n"
-				"void main( void ) {\n"
-					"vs2fs.color = vertex;\n"
-					"gl_Position = mvp * vec4( vertex, 1. );"
-				"}\n",
+                //Vertex Shader
+                "#version 330 core\n"
+                "layout( location = 0 ) in vec3 vertex;\n"
+                "uniform mat4 mvp;\n"
+                "out VS2FS {\n"
+                "	vec3 color;\n"
+                "} vs2fs;\n"
+                "void main( void ) {\n"
+                    "vs2fs.color = vertex;\n"
+                    "gl_Position = mvp * vec4( vertex, 1. );"
+                "}\n",
 
-				//Fragment Shader
-				"#version 330 core\n"
-				"in VS2FS {\n"
-				"	vec3 color;\n"
-				"} vs2fs;\n"
-				"out vec4 fColor;\n"
-				"void main( void ) {\n"
-					"fColor = vec4( vs2fs.color, 1. );\n"
-				"}\n",
-				GLR::ShaderCode::FROM_CODE ).
-				addUniform( "mvp", GLR::Shader::MAT4, GLR::Shader::SCALAR, & mvp );
-		}
-	}
+                //Fragment Shader
+                "#version 330 core\n"
+                "in VS2FS {\n"
+                "	vec3 color;\n"
+                "} vs2fs;\n"
+                "out vec4 fColor;\n"
+                "void main( void ) {\n"
+                    "fColor = vec4( vs2fs.color, 1. );\n"
+                "}\n",
+                GLR::ShaderCode::FROM_CODE ).
+                addUniform( "mvp", GLR::Shader::MAT4, GLR::Shader::SCALAR, & mvp );
+        }
+    }
 
-	// container
-	{
-		// C-DUMMY-TRIANGLE
-		{
-			glr.container( "C-DUMMY-TRIANGLE" ).
-				setVertexArray( "V-DUMMY-TRIANGLE" ).
-				setShader( "S-DUMMY-TRIANGLE" ).
-				build( );
-		}
-	}
+    // container
+    {
+        // C-DUMMY-TRIANGLE
+        {
+            glr.container( "C-DUMMY-TRIANGLE" ).
+                setVertexArray( "V-DUMMY-TRIANGLE" ).
+                setShader( "S-DUMMY-TRIANGLE" ).
+                build( );
+        }
+    }
 
-	p = v = m = glm::mat4( 1. );
+    p = v = m = glm::mat4( 1. );
 
-	glDisable( GL_DEPTH_TEST );
-	glDisable( GL_CULL_FACE );
+    glEnable( GL_DEPTH_TEST );
+    glDisable( GL_CULL_FACE );
 }
 
 void
 Dummy::paint( ) {
 
-	float
-	angle = .15f * vcd->time;
+    float
+    angle = .15f * vcd->time;
 
-	v = glm::mat4( 1. );
-	v = glm::translate( glm::mat4( 1. ), glm::vec3( 0.f, 0.f, -2.f ) );
-	v = glm::rotate( v, angle, glm::vec3( 0.f, 1.f, 0.f ) );
+    v = glm::mat4( 1. );
+    v = glm::translate( glm::mat4( 1. ), glm::vec3( 0.f, 0.f, -2.f ) );
+    v = glm::rotate( v, angle, glm::vec3( 0.f, 1.f, 0.f ) );
 
-	mvp = p * v * m;
+    mvp = p * v * m;
 
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-	glr.run( { "C-DUMMY-TRIANGLE" } );
+    glr.run( { "C-DUMMY-TRIANGLE" } );
 }
 
 void
 Dummy::resize( int p_width, int p_height ) {
 
-	float
-	w = p_width,
-	h = p_height,
-	ratio = w / h;
+    float
+    w = p_width,
+    h = p_height,
+    ratio = w / h;
 
-	p = glm::perspective(  45.0f, ratio, 1.0f, 100.f );
+    p = glm::perspective(  45.0f, ratio, 1.0f, 100.f );
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 }
--- glprojects/dummy/dummy.hpp
+++ glprojects/dummy/dummy.hpp
@@ -5,125 +5,167 @@
 class
 Triangle {
 
-	public:
+    public:
 
-		Triangle ( glm::vec3 const & p_a, glm::vec3 const & p_b, glm::vec3 const & p_c ) :
-		a ( p_a ),
-		b ( p_b ),
-		c ( p_c ),
-		t ( nullptr ) {
+        Triangle ( glm::vec3 const & p_a, glm::vec3 const & p_b, glm::vec3 const & p_c ) :
+        a ( p_a ),
+        b ( p_b ),
+        c ( p_c ),
+        t ( nullptr ) {
 
-		}
+        }
 
-		~Triangle ( ) {
+        ~ Triangle ( ) {
 
-			if ( ! t )
+            if ( ! t )
 
-				return;
+                return;
 
-			for ( GLuint i = 0; i < 4; ++ i ) {
+            for ( GLuint i = 0; i < 4; ++ i ) {
 
-				delete t [ i ];
+                delete t [ i ];
 
-				t [ i ] = nullptr;
-			}
+                t [ i ] = nullptr;
+            }
 
-			delete [ ] t;
+            delete [ ] t;
 
-			t = nullptr;
-		}
+            t = nullptr;
+        }
 
-		glm::vec3
-		a,
-		b,
-		c;
+        glm::vec3
+        a,
+        b,
+        c;
 
-		Triangle
-		* * t;
+        Triangle
+        * * t;
 
-	public:
+    public:
 
-		void
-		divide ( GLsizei p_count ) {
+        void
+        divide ( GLsizei p_count ) {
 
-			if ( -- p_count < 0 ) {
+            if ( -- p_count < 0 ) {
 
-				return;
-			}
+                return;
+            }
 
-			glm::vec3
-			ab = 1.f * ( .51f * a + .49f * b ),
-			bc = 1.f * ( .51f * b + .49f * c ),
-			ca = 1.f * ( .51f * c + .49f * a ),
-			ba = 1.f * ( .49f * a + .51f * b ),
-			cb = 1.f * ( .49f * b + .51f * c ),
-			ac = 1.f * ( .49f * c + .51f * a ),
-			ab2 = 1.f * ( .5f * a + .5f * b ),
-			bc2 = 1.f * ( .5f * b + .5f * c ),
-			ca2 = 1.f * ( .5f * c + .5f * a );
+            glm::vec3
+            ab = 1.f * ( .51f * a + .49f * b ),
+            bc = 1.f * ( .51f * b + .49f * c ),
+            ca = 1.f * ( .51f * c + .49f * a ),
+            ba = 1.f * ( .49f * a + .51f * b ),
+            cb = 1.f * ( .49f * b + .51f * c ),
+            ac = 1.f * ( .49f * c + .51f * a ),
+            ab2 = 1.f * ( .5f * a + .5f * b ),
+            bc2 = 1.f * ( .5f * b + .5f * c ),
+            ca2 = 1.f * ( .5f * c + .5f * a );
 
-			t = new Triangle * [ 4 ];
+            t = new Triangle * [ 4 ];
 
-			t [ 0 ] = new Triangle ( a, ab, ac );
-			t [ 1 ] = new Triangle ( b, bc, ba );
-			t [ 2 ] = new Triangle ( c, ca, cb );
-			t [ 3 ] = new Triangle ( ab2, bc2, ca2 );
+            t [ 0 ] = new Triangle ( a, ab, ac );
+            t [ 1 ] = new Triangle ( b, bc, ba );
+            t [ 2 ] = new Triangle ( c, ca, cb );
+            t [ 3 ] = new Triangle ( ab2, bc2, ca2 );
 
-			t [ 0 ] -> divide ( p_count );
-			t [ 1 ] -> divide ( p_count );
-			t [ 2 ] -> divide ( p_count );
-			t [ 3 ] -> divide ( p_count );
-		}
+            t [ 0 ] -> divide ( p_count );
+            t [ 1 ] -> divide ( p_count );
+            t [ 2 ] -> divide ( p_count );
+            t [ 3 ] -> divide ( p_count );
+        }
 
-		GLR::VertexArray
-		& fill ( GLR::VertexArray & p_va ) {
+        GLR::VertexArray
+        & fill ( GLR::VertexArray & p_va ) {
 
-			if ( t ) {
+            if ( t ) {
 
-				for ( GLsizei i = 0; i < 4; ++ i ) {
+                for ( GLsizei i = 0; i < 4; ++ i ) {
 
-					t [ i ] -> fill ( p_va );
-				}
-			}
-			else {
+                    t [ i ] -> fill ( p_va );
+                }
+            }
+            else {
 
-				p_va << a << b << c;
-			}
+//                p_va << glm::normalize( a ) << glm::normalize( b ) << glm::normalize( c );
+                p_va << a << b << c;
+            }
 
-			return p_va;
-		}
+            return p_va;
+        }
+};
+
+class
+Tetrahedron {
+
+    public:
+
+        Tetrahedron ( Triangle const & p_a, Triangle const & p_b, Triangle const & p_c, Triangle const & p_d ) :
+        a ( p_a ),
+        b ( p_b ),
+        c ( p_c ),
+        d ( p_d ) {
+
+        }
+
+        ~ Tetrahedron ( ) {
+
+        }
+
+        Triangle
+        a,
+        b,
+        c,
+        d;
+
+    public:
+
+        void
+        divide ( GLsizei p_count ) {
+
+            a.divide( p_count );
+            b.divide( p_count );
+            c.divide( p_count );
+            d.divide( p_count );
+        }
+
+        GLR::VertexArray
+        & fill ( GLR::VertexArray & p_va ) {
+
+            return d.fill( c.fill( b.fill( a.fill( p_va ) ) ) );
+        }
 };
 
 class Dummy :
 public GLProject {
 
-	public:
+    public:
 
-		Dummy ( CStr const & p_name = "Dummy", ViewControlData *p_vcd = nullptr );
+        Dummy ( CStr const & p_name = "Dummy", ViewControlData *p_vcd = nullptr );
 
-		~ Dummy( );
+        ~ Dummy( );
 
-	public:
+    public:
 
-		glm::mat4
-		m,//odel
-		v,//iew
-		p,//projection
-		mvp;//model-view-projection
+        glm::mat4
+        m,//odel
+        v,//iew
+        p,//projection
+        mvp;//model-view-projection
 
-		Triangle
-		* t;
+        Tetrahedron
+        tetrahedron;
 
-	public:
+    public:
 
-		void
-		init( );
+        void
+        init( );
 
-		void
-		paint( );
+        void
+        paint( );
 
-		void
-		resize( int p_width, int p_height );
+        void
+        resize( int p_width, int p_height );
 
 };
 
